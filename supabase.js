87ce65e20640f099a85e8af07ff43bac4815986b
earngig01 <earngigofficial@gmail.com>
2026-09-06 15:13:36 +0100

    Fix syntax error in supabase.js

--- supabase.js
+++ supabase.js
@@ -4,4 +4,4 @@ const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_CLTeqgEB6JSQ7Kqq6rQARw_RV31Ccg3
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_PUBLISHABLE_KEY
-)
+);
